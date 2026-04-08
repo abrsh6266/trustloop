cefe92e26ddc6b83beba1f4c099d286af0f9568a
Abrham Belayneh <abrsh6266@gmail.com>
2026-04-08 10:24:15 +0300

    feat: initialize TanStack Query provider and wrap application with AppProviders

---
+++ components/providers/app-providers.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import { QueryProvider } from "./query-provider";
+
+export function AppProviders({ children }: { children: ReactNode }) {
+  return <QueryProvider>{children}</QueryProvider>;
+}
